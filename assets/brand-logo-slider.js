4469fac04f23862f8dc8455f2bf60b273d694fd1
JohnE10 <csworg@gmail.com>
2024-07-17 19:58:40 +0100

    u

---
+++ assets/brand-logo-slider.js
@@ -0,0 +1,6 @@
+window.addEventListener('resize', () => {
+  console.log(`Width: ${window.innerWidth}, Height: ${window.innerHeight}`);
+});
+
+// Initial log
+console.log(`Width: ${window.innerWidth}, Height: ${window.innerHeight}`);
